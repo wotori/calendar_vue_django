2658b02b56258605fc6f7be87fa88f2b71d42d6e
Dimitri Sobolev <dmsobolev@yahoo.com>
2020-12-07 12:04:12 +0300

    callendar basic interface integrated

--- calendar_ui/README.md
+++ calendar_ui/README.md
@@ -1,21 +1,42 @@
-# calendar_ui
+# vue-simple-calendar-sample
 
-> calendar task
+> Sample app showing off the `vue-simple-calendar` component
+
+This is a demo app for the open-source `vue-simple-calendar` Vue component ([https://github.com/richardtallent/vue-simple-calendar](https://github.com/richardtallent/vue-simple-calendar)).
+
+For a live demo, visit [https://www.tallent.us/vue-simple-calendar/](https://www.tallent.us/vue-simple-calendar/).
 
 ## Build Setup
 
-``` bash
+```bash
 # install dependencies
 npm install
 
 # serve with hot reload at localhost:8080
-npm run dev
+npm run serve
 
 # build for production with minification
 npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
 ```
 
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+## Release History
+
+| Date       | Version | Notes                                                           |
+| ---------- | ------- | --------------------------------------------------------------- |
+| 2017.12.17 | 1.0.0   | Split from the main component repo (using 1.8.0)                |
+| 2017.12.30 | 1.0.1   | Upgrade component to 1.8.2                                      |
+| 2018.01.01 | 2.0.0   | Upgrade component to 2.0.0                                      |
+| 2018.01.23 | 2.0.1   | Upgrade component to 2.0.1                                      |
+| 2018.01.24 | 2.1.0   | Upgrade component to 2.1.0, adjust events listened for          |
+| 2018.01.25 | 2.1.1   | Upgrade component to 2.1.1                                      |
+| 2018.01.27 | 2.1.3   | Upgrade component to 2.1.3                                      |
+| 2018.03.18 | 2.2.0   | Upgrade component to 2.2.0                                      |
+| 2018.03.19 | 2.2.1   | Upgrade component to 2.2.1, change how mixin is used, fix babel |
+| 2018.05.16 | 3.0.2   | Upgrade component to 3.0.2                                      |
+| 2018.08.25 | 4.0.2   | Upgrade component to 4.0.2, migrate to vue-cli 3                |
+| 2018.10.05 | 4.1.0   | Upgrade component to 4.1.0                                      |
+| 2018.12.18 | 4.2.0   | Upgrade component to 4.2.0, auto-gen id (fixes #104)            |
+| 2019.01.25 | 4.2.1   | Upgrade component to 4.2.1, update other dependencies           |
+| 2019.05.01 | 4.2.2   | Upgrade component to 4.2.2, update other dependencies           |
+| 2019.11.24 | 4.3.2   | Upgrade component to 4.3.2, update other dependencies           |
+| 2020.09.04 | 5.0.0   | Upgrade component to 5.0.0, update other dependencies           |
--- calendar_ui/src/App.vue
+++ calendar_ui/src/App.vue
@@ -1,23 +1,285 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="calendar-parent">
+      <calendar-view
+          :items="items"
+          :show-date="showDate"
+          :time-format-options="{ hour: 'numeric', minute: '2-digit' }"
+          :enable-drag-drop="true"
+          :disable-past="disablePast"
+          :disable-future="disableFuture"
+          :show-times="showTimes"
+          :date-classes="myDateClasses"
+          :display-period-uom="displayPeriodUom"
+          :display-period-count="displayPeriodCount"
+          :starting-day-of-week="startingDayOfWeek"
+          :class="themeClasses"
+          :period-changed-callback="periodChanged"
+          :current-period-label="useTodayIcons ? 'icons' : ''"
+          :displayWeekNumbers="displayWeekNumbers"
+          :enable-date-selection="true"
+          :selection-start="selectionStart"
+          :selection-end="selectionEnd"
+          @date-selection-start="setSelection"
+          @date-selection="setSelection"
+          @date-selection-finish="finishSelection"
+          @drop-on-date="onDrop"
+          @click-date="onClickDay"
+          @click-item="onClickItem"
+      >
+        <calendar-view-header
+            slot="header"
+            slot-scope="{ headerProps }"
+            :header-props="headerProps"
+            @input="setShowDate"
+        />
+      </calendar-view>
+    </div>
   </div>
 </template>
-
 <script>
+require("vue-simple-calendar/static/css/default.css")
+require("vue-simple-calendar/static/css/holidays-us.css")
+
+
+import {
+  CalendarView,
+  CalendarViewHeader,
+  CalendarMathMixin,
+} from "vue-simple-calendar" // published version
+//} from "../../vue-simple-calendar/src/components/bundle.js" // local repo
+
 export default {
-  name: 'App'
+  name: "App",
+  components: {
+    CalendarView,
+    CalendarViewHeader,
+  },
+  mixins: [CalendarMathMixin],
+  data() {
+    return {
+      /* Show the current month, and give it some fake items to show */
+      showDate: this.thisMonth(1),
+      message: "",
+      startingDayOfWeek: 0,
+      disablePast: false,
+      disableFuture: false,
+      displayPeriodUom: "month",
+      displayPeriodCount: 1,
+      displayWeekNumbers: false,
+      showTimes: true,
+      selectionStart: null,
+      selectionEnd: null,
+      newItemTitle: "",
+      newItemStartDate: "",
+      newItemEndDate: "",
+      useDefaultTheme: true,
+      useHolidayTheme: true,
+      useTodayIcons: false,
+      items: [
+        {
+          id: "e0",
+          startDate: "2018-01-05",
+        },
+        {
+          id: "e1",
+          startDate: this.thisMonth(15, 18, 30),
+        },
+        {
+          id: "e2",
+          startDate: this.thisMonth(15),
+          title: "Один день с длинным титулом",
+        },
+        {
+          id: "e3",
+          startDate: this.thisMonth(7, 9, 25),
+          endDate: this.thisMonth(10, 16, 30),
+          title: "Выделение нескольких дней",
+        },
+        {
+          id: "e4",
+          startDate: this.thisMonth(20),
+          title: "Ссылка",
+          classes: "polymorphism",
+          url: "https://en.wikipedia.org/wiki/Polymorphism_(computer_science)",
+        },
+        {
+          id: "e5",
+          startDate: this.thisMonth(5),
+          endDate: this.thisMonth(12),
+          title: "событие - несколько дней",
+          classes: "purple",
+        },
+        {
+          id: "foo",
+          startDate: this.thisMonth(29),
+          title: "Задача №1",
+        },
+        {
+          id: "e6",
+          startDate: this.thisMonth(29),
+          title: "Задача №2",
+          classes: "orange",
+        }
+      ],
+    }
+  },
+  computed: {
+    userLocale() {
+      return this.getDefaultBrowserLocale
+    },
+    dayNames() {
+      return this.getFormattedWeekdayNames(this.userLocale, "long", 0)
+    },
+    themeClasses() {
+      return {
+        "theme-default": this.useDefaultTheme,
+        "holiday-us-traditional": this.useHolidayTheme,
+        "holiday-us-official": this.useHolidayTheme,
+      }
+    },
+    myDateClasses() {
+      // This was added to demonstrate the dateClasses prop. Note in particular that the
+      // keys of the object are `yyyy-mm-dd` ISO date strings (not dates), and the values
+      // for those keys are strings or string arrays. Keep in mind that your CSS to style these
+      // may need to be fairly specific to make it override your theme's styles. See the
+      // CSS at the bottom of this component to see how these are styled.
+      const o = {}
+      const theFirst = this.thisMonth(1)
+      const ides = [2, 4, 6, 9].includes(theFirst.getMonth()) ? 15 : 13
+      const idesDate = this.thisMonth(ides)
+      o[this.isoYearMonthDay(idesDate)] = "ides"
+      o[this.isoYearMonthDay(this.thisMonth(21))] = [
+        "do-you-remember",
+        "the-21st",
+      ]
+      return o
+    },
+  },
+  mounted() {
+    this.newItemStartDate = this.isoYearMonthDay(this.today())
+    this.newItemEndDate = this.isoYearMonthDay(this.today())
+  },
+
+  methods: {
+    periodChanged() {
+      // range, eventSource) {
+      // Demo does nothing with this information, just including the method to demonstrate how
+      // you can listen for changes to the displayed range and react to them (by loading items, etc.)
+      //console.log(eventSource)
+      //console.log(range)
+    },
+    thisMonth(d, h, m) {
+      const t = new Date()
+      return new Date(t.getFullYear(), t.getMonth(), d, h || 0, m || 0)
+    },
+    onClickDay(d) {
+      this.selectionStart = null
+      this.selectionEnd = null
+      this.message = `You clicked: ${d.toLocaleDateString()}`
+    },
+    onClickItem(e) {
+      this.message = `You clicked: ${e.title}`
+    },
+    setShowDate(d) {
+      this.message = `Changing calendar view to ${d.toLocaleDateString()}`
+      this.showDate = d
+    },
+    setSelection(dateRange) {
+      this.selectionEnd = dateRange[1]
+      this.selectionStart = dateRange[0]
+    },
+    finishSelection(dateRange) {
+      this.setSelection(dateRange)
+      this.message = `You selected: ${this.selectionStart.toLocaleDateString()} -${this.selectionEnd.toLocaleDateString()}`
+    },
+    onDrop(item, date) {
+      this.message = `You dropped ${item.id} on ${date.toLocaleDateString()}`
+      // Determine the delta between the old start date and the date chosen,
+      // and apply that delta to both the start and end date to move the item.
+      const eLength = this.dayDiff(item.startDate, date)
+      item.originalItem.startDate = this.addDays(item.startDate, eLength)
+      item.originalItem.endDate = this.addDays(item.endDate, eLength)
+    },
+    clickTestAddItem() {
+      this.items.push({
+        startDate: this.newItemStartDate,
+        endDate: this.newItemEndDate,
+        title: this.newItemTitle,
+        id: "e" + Math.random().toString(36).substr(2, 10),
+      })
+      this.message = "You added a calendar item!"
+    },
+  },
 }
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+  margin: 0;
+  background-color: #f7fcff;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: row;
+  font-family: Calibri, sans-serif;
+  width: 95vw;
+  min-width: 30rem;
+  max-width: 100rem;
+  min-height: 40rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.calendar-controls {
+  margin-right: 1rem;
+  min-width: 14rem;
+  max-width: 14rem;
+}
+
+.calendar-parent {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  max-height: 80vh;
+  background-color: white;
+}
+
+/* For long calendars, ensure each week gets sufficient height. The body of the calendar will scroll if needed */
+.cv-wrapper.period-month.periodCount-2 .cv-week,
+.cv-wrapper.period-month.periodCount-3 .cv-week,
+.cv-wrapper.period-year .cv-week {
+  min-height: 6rem;
+}
+
+/* These styles are optional, to illustrate the flexbility of styling the calendar purely with CSS. */
+
+/* Add some styling for items tagged with the "birthday" class */
+.theme-default .cv-item.birthday {
+  background-color: #e0f0e0;
+  border-color: #d7e7d7;
+}
+
+.theme-default .cv-item.birthday::before {
+  content: "\1F382"; /* Birthday cake */
+  margin-right: 0.5em;
+}
+
+/* The following classes style the classes computed in myDateClasses and passed to the component's dateClasses prop. */
+.theme-default .cv-day.ides {
+  background-color: #ffe0e0;
+}
+
+.ides .cv-day-number::before {
+  content: "\271D";
+}
+
+.theme-default .cv-day.do-you-remember.the-21st .cv-day-number::after {
+  content: "\1F30D\1F32C\1F525";
 }
 </style>
